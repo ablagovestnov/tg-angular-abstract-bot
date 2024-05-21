34b8f75073f61edc6763eec7a2478d002c7043e6
Alexandr Blagovestnov <a.blagovestnov@dcoa.ru>
2024-05-21 20:39:08 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import { Telegraf, Markup } from 'telegraf'
+import { message } from 'telegraf/filters'
+
+const token = '7012022502:AAG6rOFhryq0xE8NKZ5pe0ZN3n5Fsee4grs'
+const webAppUrl = 'https://ablagovestnov.github.io/tg-angular-abstract-app/'
+
+const bot = new Telegraf(token)
+
+bot.command('start', (ctx) => {
+    ctx.reply(
+        'Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение',
+        Markup.keyboard([
+            Markup.button.webApp('Отправить сообщение', `${webAppUrl}`),
+        ])
+    )
+})
+bot.launch()
